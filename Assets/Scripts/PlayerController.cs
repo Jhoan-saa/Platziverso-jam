f779558637d2254c95187ffd38bff70c893e8bab
Quiezel <reveles.rosalio@gmail.com>
2022-05-26 19:36:53 -0500

    solo formato de tabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    //Declaramos los valores minimos y maximos de X y la velocidad de movimiento
-    public float speed = 10f;
     private float minValueX = -7f;
     private float maxValueX = 7f;
+    public float speed = 10f;
     public bool forceUp = true;
     public float forceUpVelocity = 100f;
-  
 
     
     // Start is calld before the first frame update
     void Start()
     {
-       
-        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-         // declaramos el control de movimiento derecha izquierda
-         transform.Translate(Vector2.right*speed*Time.deltaTime*Input.GetAxis("Horizontal"));
-         //condicionamos los limites en X en positivo
-         if(transform.position.x >7f)
-         {
-             transform.position = new Vector2(maxValueX,transform.position.y);
+        // declaramos el control de movimiento derecha izquierda
+        transform.Translate(Vector2.right*speed*Time.deltaTime*Input.GetAxis("Horizontal"));
+        //condicionamos los limites en X en positivo
+        if(transform.position.x >7f)
+        {
+            transform.position = new Vector2(maxValueX,transform.position.y);
     
         }
         //condicionamos los limites en X en positivo
@@ -48,6 +45,5 @@ public class PlayerController : MonoBehaviour
     {
         forceUp = false;
     }
-  
 
 }
