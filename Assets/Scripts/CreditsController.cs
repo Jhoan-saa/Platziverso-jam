a6d22e3bc9d4c6d6cf8d2cd0134d5f08ac4cfa5c
Quiezel <reveles.rosalio@gmail.com>
2022-05-30 01:36:28 -0500

    manejo de las escenas

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CreditsController : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+
+    // Update is called once per frame
+    void LoadScene(VideoPlayer vp)
+    {
+            SceneController.toMenu();
+    }
+}
